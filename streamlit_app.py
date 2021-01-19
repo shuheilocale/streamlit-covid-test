955af652baa1eb7605ecd6ad335e4d50fff90a90
shuheilocale <checkyourheadd@gmail.com>
2021-01-19 09:10:35 +0900

    Update display

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import json
 import geopandas as gpd
 import plotly.express as px
@@ -15,14 +16,18 @@ jpn.set_index('id', drop=False, inplace=True)
 fig = px.choropleth_mapbox(jpn, geojson=json.loads(jpn['geometry'].to_json()), 
     locations='id', 
     color='id',
-    title = '人口密度',
+    title = 'covid',
     hover_name = 'nam_ja',
     color_continuous_scale="OrRd", 
     mapbox_style='white-bg',
     zoom=4, 
     center=CENTER,
     opacity=0.5,
-    labels={'DENSITY':'人口密度'}
+    labels={'id':'テスト'}
     )
+
+
+fig.update_geos(fitbounds="locations", visible=False)
 fig.update_layout(margin={'r':10,'t':40,'l':10,'b':10,'pad':5})
-fig.show()
+#fig.show()
+st.plotly_chart(fig)
